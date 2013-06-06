163a5e7f1a3d340035f2e1e40277a590d08522d4
Christopher Sexton <csexton@gmail.com>
2013-06-06 12:20:57 -0400

    Refactor slide and bump ruby version

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -2,17 +2,12 @@ class Slide
   attr_reader :body, :id
 
   def self.all
-    a = []
-    i = 0
-    list_files.each do |f|
-      read_file(f).split("!SLIDE").each do |contents|
+    list_files.each_with_object([]) do |f, slides|
+      File.read(f).split("!SLIDE").each do |contents|
         contents.strip!
-        next if contents.empty?
-        a << self.new(i, contents)
-        i+=1
+        slides << self.new(slides.length, contents) unless contents.empty?
       end
     end
-    a
   end
 
   def self.list_files
@@ -30,10 +25,6 @@ class Slide
 
   def build(body)
     @body = process_markdown(body).html_safe
-  end
-
-  def self.read_file(path)
-    File.open(path,"r").read()
   end
 
   def process_markdown(md)
